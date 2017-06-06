e61bf1e9a349659ac46396b1313f8403284135f9
IvayloDamyanov <ivaylo.damyanov@gmail.com>
2017-06-06 18:59:01 +0300

    Added JSON reader

---
+++ FootballDatabase/JSONReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Data.OleDb;
+using System.Data;
+using System.Web.Script.Serialization;
+using FootballDatabase.Models;
+
+namespace FootballDatabase
+{
+    static class JSONReader
+    {
+        private static JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+        public static Country[] ReadCountries(string filePath) {
+            Country[] countries;
+
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                string fileContent = streamReader.ReadToEnd();
+                countries = serializer.Deserialize<Country[]>(fileContent);
+            }
+
+            return countries;
+        }
+    }
+}
