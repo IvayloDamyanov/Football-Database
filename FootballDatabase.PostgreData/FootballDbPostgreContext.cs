c5e3e4b0c03cbab94d8c8620ff2dcf342dbfd8a3
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-06-11 08:59:57 +0300

    Added Postgre db.

---
+++ FootballDatabase.PostgreData/FootballDbPostgreContext.cs
@@ -0,0 +1,27 @@
+﻿namespace FootballDatabase.PostgreData
+{
+    using FootballDatabase.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class FootballDbPostgreContext : DbContext
+    {
+        public FootballDbPostgreContext()
+            : base("PgsqlDb")
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Town> Towns { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FootballDatabase.PostgreData/Migrations/201706110556375_Postgre Initial.cs
@@ -0,0 +1,80 @@
+namespace FootballDatabase.PostgreData.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PostgreInitial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Countries",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Towns",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        CountryId = c.Int(nullable: false),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Countries", t => t.CountryId, cascadeDelete: true)
+                .Index(t => t.CountryId);
+            
+            CreateTable(
+                "dbo.Teams",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        TownId = c.Int(nullable: false),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Towns", t => t.TownId, cascadeDelete: true)
+                .Index(t => t.TownId);
+            
+            CreateTable(
+                "dbo.Players",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Age = c.Int(nullable: false),
+                        Nationality = c.String(maxLength: 50),
+                        Salary = c.Decimal(precision: 18, scale: 2),
+                        Contract = c.Int(),
+                        TeamId = c.Int(nullable: false),
+                        IsDeleted = c.Boolean(nullable: false),
+                        Position = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Teams", t => t.TeamId, cascadeDelete: true)
+                .Index(t => t.TeamId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Teams", "TownId", "dbo.Towns");
+            DropForeignKey("dbo.Players", "TeamId", "dbo.Teams");
+            DropForeignKey("dbo.Towns", "CountryId", "dbo.Countries");
+            DropIndex("dbo.Players", new[] { "TeamId" });
+            DropIndex("dbo.Teams", new[] { "TownId" });
+            DropIndex("dbo.Towns", new[] { "CountryId" });
+            DropTable("dbo.Players");
+            DropTable("dbo.Teams");
+            DropTable("dbo.Towns");
+            DropTable("dbo.Countries");
+        }
+    }
+}
