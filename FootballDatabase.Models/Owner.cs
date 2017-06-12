09b16536a80f03930271ac6229b02db717148cce
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-06-12 12:49:49 +0300

    Added Owner model.

---
+++ FootballDatabase.Models/Owner.cs
@@ -0,0 +1,47 @@
+﻿namespace FootballDatabase.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Owner
+    {
+        private const int MinNameLength = 2;
+        private const int MaxNameLength = 50;
+        private const int MinAge = 30;
+        private const int MaxAge = 80;
+        private const double MinIncomes = 100000;
+
+        public Owner()
+        {
+            this.IsDeleted = false;
+            this.Teams = new HashSet<Team>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(MinNameLength)]
+        [MaxLength(MaxNameLength)]
+        public string Name { get; set; }
+
+        [MinLength(MinNameLength)]
+        [MaxLength(MaxNameLength)]
+        public string Nationality { get; set; }
+
+        [Range(MinAge, MaxAge)]
+        public int Age { get; set; }
+
+        [Range(MinIncomes, Double.MaxValue)]
+        public decimal Incomes { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public virtual ICollection<Team> Teams { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($@"Id: {this.Id}, Name: {this.Name}, Age: {this.Age}, Nationality: {this.Nationality}, Incomes: {this.Incomes}");
+        }
+    }
+}
